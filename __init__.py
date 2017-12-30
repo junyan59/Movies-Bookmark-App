8bad699e5a2ababdd2f38e660bac819b00269b5e
Jun Yan <stephanieyan59@gmail.com>
2017-12-30 12:33:34 -0500

    feat: Implement google logout feature

--- __init__.py
+++ __init__.py
@@ -245,7 +245,25 @@ def gconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect():
-    return "This will log you out. (Google)"
+    # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps(
+            'Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
 
 @app.route('/fbconnect', methods=['POST'])
